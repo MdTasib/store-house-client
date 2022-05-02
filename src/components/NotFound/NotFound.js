6b2634f7dcd834f927a6ee8f6d49b29248746d20
MdTasib <ohidulalam442@gmail.com>
2022-05-02 22:36:25 +0600

    404 page implement

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import notFound from "../../assets/images/notFound.gif";
 
 const NotFound = () => {
+	const navigate = useNavigate();
 	return (
 		<div>
-			<h3>page note found</h3>
+			<img src={notFound} className='w-100' style={{ height: "80vh" }} alt='' />
+			<div className='text-center bg-black'>
+				<button
+					onClick={() => navigate("/")}
+					className='btn btn-outline-dark text-white'>
+					GO TO HOME
+				</button>
+			</div>
 		</div>
 	);
 };
